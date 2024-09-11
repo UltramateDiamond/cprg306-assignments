ed60bb5fc7c5bddff3d10c025267c65ee4ae05d2
UltramateDiamond <166836992+UltramateDiamond@users.noreply.github.com>
2024-09-11 10:34:39 -0600

    Finished week 2 assignment
    Completed steps 1-4

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,13 +1,14 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function StudentInfo() {
   return (
     <main>
+      <div>Yichen Hao</div>
       <Link
         className="underline text-cyan-600 hover:text-cyan-300"
-        href="./week-2/"
+        href="https://github.com/UltramateDiamond/cprg306-assignments"
       >
-        Week 2
+        My Github Repository
       </Link>
     </main>
   );
