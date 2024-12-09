ca0f77db67690c58b0c0432aaca226a30eafc947
UltramateDiamond <166836992+UltramateDiamond@users.noreply.github.com>
2024-12-09 11:34:54 -0700

    week-6 updated

--- app/week-7/item-list.js
+++ app/week-7/item-list.js
@@ -8,48 +8,33 @@ import itemsData from "./items.json";
 export default function ItemList(){
 
     const[sortBy, setSortBy] = useState("name");
-    const[items, setItems] = useState(itemsData);
 
-    const sortItems = () => {
-        const sortedItems = [...items];
-
-        sortedItems.sort((a, b) =>{
-            console.log(sortBy);
-            if (sortBy === 'name') {
-                return a.name.localeCompare(b.name);
-            } else if (sortBy === 'category') {
-                return a.name.localeCompare(b.name);
-            } else {
-                return 'invalid'
-            }
-        });
-
-        return sortedItems;
-    }
-
-    const handleSortByName = () => {
-        setSortBy("name");
-        setItems(sortItems());
-    };
-
-    const handleSortByCategory = () => {
-        setSortBy("category");
-        setItems(sortItems());
-    };
+    const sortedItems = [...itemsData].sort((a, b) => {
+        if (a[sortBy] < b[sortBy]) return -1;
+        if (a[sortBy] > b[sortBy]) return 1;
+        return 0;
+      });
 
     return(
         <main>
-            <p>
-                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={handleSortByName}>Sort By Name</button>
-            </p>
-            <p>
-                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={handleSortByCategory}>Sort By Category</button>
-            </p>
-            <ul>
-                {sortItems().map((item) => ( 
-                    <Item name={item.name} quantity={item.quantity} category={item.category} /> 
-                    ))}
-            </ul>
+            <button
+          className={`px-4 py-2 mr-2 ${sortBy === "name" ? "bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" : "bg-gray-200 text-gray-700"}`}
+          onClick={() => setSortBy("name")}
+        >
+          Sort by Name
+        </button>
+        <button
+         className={`px-4 py-2 ${sortBy === "category" ? "bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" : "bg-gray-200 text-gray-700"
+        }`}
+          onClick={() => setSortBy("category")}
+        >
+          Sort by Category
+        </button>
+            <ul className="space-y-4">
+        {sortedItems.map((item) => (
+          <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} />
+        ))}
+      </ul>
         </main>
     );
 }
