af08a743866e0d2accc23ab3f8675050504456a7
UltramateDiamond <166836992+UltramateDiamond@users.noreply.github.com>
2024-12-08 21:36:19 -0700

    Incomplete

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -8,12 +8,10 @@ import itemsData from "./items.json";
 export default function ItemList(){
 
     const[sortBy, setSortBy] = useState("name");
-    const[items, setItems] = useState(itemsData);
 
     const sortItems = () => {
-        const sortedItems = [...items];
 
-        sortedItems.sort((a, b) =>{
+        const sortedItems = [...itemsData].sort((a, b) =>{
             console.log(sortBy);
             if (sortBy === 'name') {
                 return a.name.localeCompare(b.name);
@@ -27,27 +25,21 @@ export default function ItemList(){
         return sortedItems;
     }
 
-    const handleSortByName = () => {
-        setSortBy("name");
-        setItems(sortItems());
-    };
-
-    const handleSortByCategory = () => {
-        setSortBy("category");
-        setItems(sortItems());
-    };
+    const renderedItems = sortedItems.map((item) => {
+        <Item></Item>
+    })
 
     return(
         <main>
             <p>
-                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={handleSortByName}>Sort By Name</button>
+                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={() => setSortBy('name')}>Sort By Name</button>
             </p>
             <p>
-                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={handleSortByCategory}>Sort By Category</button>
+                <button className="bg-blue-400 hover:bg-blue-700 active:bg-yellow-300 rounded text-white mt-5 px-4 py-2" onClick={() => setSortBy('category')}>Sort By Category</button>
             </p>
             <ul>
                 {sortItems().map((item) => ( 
-                    <Item name={item.name} quantity={item.quantity} category={item.category} /> 
+                    <Item key={item.id} name={item.name} quantity={item.quantity} category={item.category} /> 
                     ))}
             </ul>
         </main>
